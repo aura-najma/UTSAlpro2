2ab0ff4f8703faacda96ed922d27d213d18769c2
aura-najma <140328001+aura-najma@users.noreply.github.com>
2023-10-26 11:10:56 +0700

    Update getcsv2json.php

--- getcsv2json.php
+++ getcsv2json.php
@@ -16,8 +16,8 @@ function csvToJson($csvUrl) {
     $jsonArray = [];
 
     foreach ($csvData as $row) {
-        $jsonArrayItem = array();
-        for ($i); {
+        $jsonArrayItem = array[];
+        for ($i) {
             $jsonArrayItem[$headers[$i]] = $row[$i];
         }
         $jsonArray[] = $jsonArrayItem;
@@ -26,7 +26,7 @@ function csvToJson($csvUrl) {
     return json_encode($jsonArray);
 }
 
-$csvUrl = 'https://alpro2aura.alwaysdata.net/UTSAlpro2/datapribadi.csv';
+$csvUrl = 'https://testingalpro.alwaysdata.net/api/coffee.csv';
 $jsonData = csvToJson($csvUrl);
 
 // Set the content type to JSON
