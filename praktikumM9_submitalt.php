e9cb966a1bf1fecd496b1fdeae5581b8bfcdbaac
aura-najma <140328001+aura-najma@users.noreply.github.com>
2023-11-08 16:13:37 +0700

    Add files via upload

---
+++ praktikumM9_submitalt.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['id'];
+    $f_name = $_POST['f_name'];
+    $l_name = $_POST['l_name'];
+    $email = $_POST['email'];
+    $email2 = $_POST['email2'];
+    $profesi = $_POST['profesi'];
+
+    // Format data sesuai CSV
+    $data = "$id, $f_name, $l_name, $email, $email2, $profesi\n";
+
+    // Path ke file CSV
+    $csvFile = 'https://alpro2aura.alwaysdata.net/UTSAlpro2/datapribadi.csv';
+
+    // Menambahkan data ke file CSV
+    $result = file_put_contents($csvFile, $data, FILE_APPEND);
+
+    if ($result !== false) {
+        echo "Data berhasil ditambahkan ke file CSV.";
+    } else {
+        echo "Gagal menambahkan data ke file CSV.";
+    }
+} else {
+    echo "Akses ke halaman ini tidak diizinkan.";
+}
+?>
