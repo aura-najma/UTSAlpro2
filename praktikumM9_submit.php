d7257a0fc8d3f81b0752207f7b400e687e84c730
aura-najma <140328001+aura-najma@users.noreply.github.com>
2023-11-08 15:25:18 +0700

    Update praktikumM9_submit.php

--- praktikumM9_submit.php
+++ praktikumM9_submit.php
@@ -1,26 +1,20 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = $_POST['id'];
-    $f_name = $_POST['F_name'];
-    $l_name = $_POST['L_name'];
+    $F_name = $_POST['F_name'];
+    $L_name = $_POST['L_name'];
     $email = $_POST['email'];
     $email2 = $_POST['email2'];
     $profesi = $_POST['profesi'];
 
+    $file = 'datapribadi.csv';
     // Format data sesuai CSV
     $data = "$id, $f_name, $l_name, $email, $email2, $profesi\n";
 
-    // Path ke file CSV
-    $csvFile = 'https://alpro2aura.alwaysdata.net/UTSAlpro2/datapribadi.csv';
-
     // Menambahkan data ke file CSV
-    $result = file_put_contents($csvFile, $data, FILE_APPEND);
+    file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
 
-    if ($result !== false) {
-        echo "Data berhasil ditambahkan ke file CSV.";
-    } else {
-        echo "Gagal menambahkan data ke file CSV.";
-    }
+    echo "Data berhasil ditambahkan ke file CSV.";
 } else {
     echo "Akses ke halaman ini tidak diizinkan.";
 }
